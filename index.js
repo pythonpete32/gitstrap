8c579839899ada572cc8f1fd9d30769d4d9cd7fb
pythonpete32 <pythonpete32@gmail.com>
2020-02-25 00:33:24 +0000

    impliment startup phase

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('G-Strap', { horizontalLayout: 'full' })
+  )
+);
