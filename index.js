026a3fabee9a7689f0a790f75c216ddce1a15314
pythonpete32 <pythonpete32@gmail.com>
2020-02-25 00:36:15 +0000

    add check not git repo

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const files = require('./lib/files');
 
 clear();
 
+// banner
 console.log(
-  chalk.yellow(
-    figlet.textSync('G-Strap', { horizontalLayout: 'full' })
-  )
+    chalk.yellow(
+        figlet.textSync('G-Strap', {
+            horizontalLayout: 'full'
+        })
+    )
 );
+
+// sanity check: not already a git repo
+if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a Git repository!'));
+    process.exit();
+}
